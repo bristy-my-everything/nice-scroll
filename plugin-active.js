9b489fe2dec727435a20ae08952e70ec12eb31ca
abdullah-al-mahdi <abdullahalmahdi1971@gmail.com>
2018-12-01 02:45:36 +0600

    first commit

---
+++ plugin-active.js
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////
+// 07. Nice Scroll
+if (screen.width >= 991) {
+$("body").niceScroll({
+  cursorcolor: "#FBB431",
+  cursorborder: "0px",
+  cursorwidth: "10px",
+  cursorborderradius: "3px",
+  background: "#3F3F3F"
+});
+};
