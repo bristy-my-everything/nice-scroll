a3f4f54533b4510fcc3dc8426db5ed2e56edaefe
Bristy <44986339+bristy-my-everything@users.noreply.github.com>
2018-12-01 05:08:17 +0600

    Update plugin-active.js

--- plugin-active.js
+++ plugin-active.js
@@ -1,11 +1,12 @@
 ////////////////////////////////////////////////////
 // 07. Nice Scroll
-if (screen.width >= 991) {
-$("body").niceScroll({
-  cursorcolor: "#FBB431",
-  cursorborder: "0px",
-  cursorwidth: "10px",
-  cursorborderradius: "3px",
-  background: "#3F3F3F"
-});
-};
+  if (screen.width >= 991) {
+    $("body").niceScroll({
+      cursorcolor: "#FBB431",
+      cursorborder: "0px",
+      cursorwidth: "10px",
+      cursorborderradius: "3px",
+      zindex: "auto" | [99999999],
+      background: "#3F3F3F"
+    });
+  };
